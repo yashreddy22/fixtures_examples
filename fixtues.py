8e4013c960f8b3faf6bd518389950fb71ab1abb1
yashreddy22 <140683448+yashreddy22@users.noreply.github.com>
2024-01-19 15:50:39 +0530

    .

---
+++ fixtues.py
@@ -0,0 +1,32 @@
+import pytest
+
+@pytest.fixture
+def value():
+	num=20
+	return num
+
+def test_add(add):
+	assert num==20
+
+
+
+import pytest
+
+@pytest.fixture
+def user():
+	return{
+		"username":"yaswanth nikhil",
+		"email":"yaswanth@gmail.com",
+		"age":23
+	{
+
+def test_user_name(user):
+	assert user["username"]=="yaswanth nikhil"
+
+def test_user_mail(user):
+	assert user["email"]=="yaswanth@gmail.com"
+
+def test_user_age(user):
+	assert user["age"]==23
+
+
